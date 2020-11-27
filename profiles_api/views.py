5538c700fbb00bbe877f57d28d087aeeb1e7d8f5
vaibhav.a.tiwari <vaibhav.a.tiwari@accenture.com>
2020-11-27 16:05:20 +0530

    added helloapiview

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format= None):
+        """returns a list of API View"""
+        an_apiView = [
+            'useus HTTp models',
+            'is similar to traditional django view',
+            'Gives you the most control over the application logic'
+        ]
+        return Response({'message':'hello','an_apiview':an_apiView})
+
+
